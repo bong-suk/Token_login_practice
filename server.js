f18587628ec0895b7d551bd02f6209dec4676f9d
bong-suk <greedi9468@hanmail.net>
2024-12-17 10:27:05 +0900

    4일차 node 과제

--- server.js
+++ server.js
@@ -41,8 +41,10 @@ app.post("/", (req, res) => {
     res.status(401).send("로그인 실패");
   } else {
     // 1. 유저정보가 있는 경우 accessToken을 발급하는 로직을 작성하세요.(sign)
+    const accessToken = jwt.sign(userInfo, secretKey, { expiresIn: "1h" });
     // 이곳에 코드를 작성하세요.
     // 2. 응답으로 accessToken을 클라이언트로 전송하세요. (res.send 사용)
+    res.send(accessToken);
     // 이곳에 코드를 작성하세요.
   }
 });
@@ -50,8 +52,11 @@ app.post("/", (req, res) => {
 // 클라이언트에서 get 요청을 받은 경우
 app.get("/", (req, res) => {
   // 3. req headers에 담겨있는 accessToken을 검증하는 로직을 작성하세요.(verify)
+  const accessToken = req.headers.authorization.split(" ")[1];
+  const decoded = jwt.verify(accessToken, secretKey);
   // 이곳에 코드를 작성하세요.
   // 4. 검증이 완료되면 유저정보를 클라이언트로 전송하세요.(res.send 사용)
+  res.send(decoded);
   // 이곳에 코드를 작성하세요.
 });
 
